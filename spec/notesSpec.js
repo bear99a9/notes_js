8d538479177fcf9e2446708bfd1604cb4a8817e8
twigz826 <thomastwigden@gmail.com>
2021-02-16 12:32:44 -0300

    Updated spec and notes, tests passing

--- spec/notesSpec.js
+++ spec/notesSpec.js
@@ -1,7 +1,25 @@
 describe('Notes', function () {
   it('checks if the user has created a note', () => {
     let notes = new Notes();
-    notes.createMessage(`Hi Salar`)
-    isEqual(notes.messages(), `Hi Salar`);
+    notes.createMessage('Hi Salar')
+    isEqual(notes.displayLastMessage(), "Hi Salar");
   });
+
+  // it('displays all messages that have been created', function(){
+  //   let notes = new Notes();
+  //   notes.createMessage(`Hi Salar`)
+  //   notes.createMessage(`Hi Sean`)
+  //   notes.createMessage(`Hi Tom`)
+  //   isEqual(notes.this.message.length, 3);
+  // })
+
+  it('displays all messages that have been created', function(){
+    let notes = new Notes();
+    notes.createMessage(`Hi Salar`)
+    isEqual(notes.displayLastMessage(), "Hi Salar"); // Hi Salar,Hi Sean,Hi Tom
+    notes.createMessage(`Hi Sean`)
+    isEqual(notes.displayLastMessage(), "Hi Sean");
+    notes.createMessage(`Hi Tom`)
+    isEqual(notes.displayLastMessage(), "Hi Tom");
+  })
 })
--- src/notes.js
+++ src/notes.js
@@ -2,10 +2,19 @@ class Notes {
     constructor() {
         this.message = [];
     }
+
     createMessage(text) {
         this.message.push(text);
     }
-    messages() {
-        return this.message[0]
+
+    displayLastMessage() {
+        return this.message[this.message.length - 1]
+        // for(let i = 0; i < this.message.length; i++) {
+        //   return this.message[i];
+        // }
+        // this.message.forEach(function(element){
+        //   return element
+        // });
     }
+
 }
